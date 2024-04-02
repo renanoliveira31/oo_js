130a398650e331644b2cc5880690d725aee3c2c2
Renan Oliveira <renanfeoliveira1@gmail.com>
2024-04-02 17:50:30 -0300

    adicionada mais 1 classe

--- carros.js
+++ carros.js
@@ -23,8 +23,33 @@ function Carro(marca, modelo, cor, custo) {
     }
 
     this.aumentoInflacao = function () {
-        const novoCusto = this.getCusto() * 1.1;
+        const novoCusto = this.getCusto() * 1.3;
         _custo = novoCusto;
+    }
+}
+
+function Bicicleta(marca, modelo, cor, aro, custo) {
+    Veiculo.call(this, marca, modelo, cor);
+    this.aro = aro;
+
+    let _custo = custo;
+    this.getCusto = function() {
+        return _custo;
+    }
+
+    this.setCusto = function(valor) {
+        if(typeof valor === 'number') {
+            _custo = valor;
+        }   
+    }
+
+    this.aumentoInflacao = function () {
+        const novoCusto = this.getCusto() * 1.7;
+        _custo = novoCusto;
+    }
+
+    this.descricao = function() {
+        return `Bicicleta ${this.marca} ${this.modelo}, aro ${this.aro}`;
     }
 }
 
@@ -45,7 +70,7 @@ function Moto(marca, modelo, cilindradas, custo) {
     }
 
     this.aumentoInflacao = function () {
-        const novoCusto = this.getCusto() * 1.1;
+        const novoCusto = this.getCusto() * 1.05;
         _custo = novoCusto;
     }
     
@@ -56,9 +81,13 @@ function Moto(marca, modelo, cilindradas, custo) {
 
 const carro1 = new Carro('Toyota', 'Corolla', 'Prata', 20000);
 const moto1 = new Moto('Yamaha', 'MT-07', '700', 10000);
+const bicicleta1 = new Bicicleta('Caloi', 'Andes', 'branca', '29', 3500);
 
 carro1.aumentoInflacao();
 console.log(carro1.descricao(),carro1.getCusto()); 
 
 moto1.aumentoInflacao();
-console.log(moto1.descricao(),moto1.getCusto()); 
+console.log(moto1.descricao(),moto1.getCusto());
+
+moto1.aumentoInflacao();
+console.log(bicicleta1.descricao(),bicicleta1.getCusto()); 
